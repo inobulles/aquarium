aa3ae5d800b87388a0d2ad0e09748b56ae275333
obiwac <obiwac@gmail.com>
2021-10-22 22:00:21 +0200

    Updated to work with new aquaBSD core releases

--- build-installer.sh
+++ build-installer.sh
@@ -2,27 +2,30 @@
 set -e
 
 rootfs="rootfs/"
+dist="dist/"
 src="src/"
 out="out/"
 
 mkdir -p $src
 
+chflags -R noschg $rootfs
 rm -rf $rootfs $out
 mkdir $rootfs $out
 
 # extract the source (we need some files from there which are not included in the distributions)
 
-tar -xf dist/src.txz -C $src
+tar -xf $dist/src.tar.gz -C $src
+mv $src/*/* $src
 
 # extract the kernel and base distributions to our rootfs
 
-tar -xf dist/kernel.txz -C $rootfs
-tar -xf dist/base.txz -C $rootfs
+tar -xf $dist/kernel.txz -C $rootfs
+tar -xf $dist/base.txz -C $rootfs
 
 # set up all the other stuff we need to create a functionnal installer image
 
 ln -s "/tmp/installer/resolv.conf" $rootfs/etc/resolv.conf
-cp $src/usr/src/release/rc.local $rootfs/etc
+cp $src/release/rc.local $rootfs/etc
 
 echo "sendmail_enable=\"NONE\"" > $rootfs/etc/rc.conf
 echo "hostid_enable=\"NO\"" >> $rootfs/etc/rc.conf
@@ -47,7 +50,7 @@ makefs -B little -o label=$label -o version=2 $image.part $rootfs
 
 # make EFI System Partition (ESP)
 
-. $src/usr/src/tools/boot/install-boot.sh # include a bunch of helpful functions
+. $src/tools/boot/install-boot.sh # include a bunch of helpful functions
 
 esp_image="$out/esp.img"
 make_esp_file $esp_image $fat32min $rootfs/boot/loader.efi

bbfd3c4d3cbb883ecadc6f4f6f2b075a31a0d0a7
obiwac <obiwac@gmail.com>
2021-11-04 20:49:28 +0100

    Separated out a `create-image.sh` script

--- bob.sh
+++ bob.sh
@@ -10,7 +10,6 @@ version="v1021a-beta"
 rootfs="rootfs"
 dist="dist"
 src="src"
-out="out"
 
 # download missing components
 
@@ -27,8 +26,8 @@ if [ -d $rootfs ]; then
 	chflags -R noschg $rootfs
 fi
 
-rm -rf $rootfs $out
-mkdir $rootfs $out
+rm -rf $rootfs
+mkdir $rootfs
 
 # extract the source (we need some files from there which are not included in the distributions)
 
@@ -90,37 +89,6 @@ rm -rf $rootfs/tmp/*
 rm $rootfs/etc/resolv.conf
 ln -s "/tmp/installer/resolv.conf" $rootfs/etc/resolv.conf
 
-# make final UFS filesystem image
+# create final image
 
-echo "[BOB] Creating UFS filesystem image ..."
-
-label="aquabsd-installer"
-image="$out/aquabsd.img"
-
-echo "/dev/ufs/$label / ufs ro,noatime 1 1" > $rootfs/etc/fstab
-echo "root_rw_mount=\"NO\"" > $rootfs/etc/rc.conf.local
-
-makefs -B little -o label=$label -o version=2 $image.part $rootfs
-
-# make EFI System Partition (ESP)
-
-echo "[BOB] Creating ESP image ..."
-
-. $src/tools/boot/install-boot.sh # include a bunch of helpful functions
-
-esp_image="$out/esp.img"
-make_esp_file $esp_image $fat32min $rootfs/boot/loader.efi
-
-# assemble final system image
-
-echo "[BOB] Assembling final image ..."
-
-mkimg -s mbr -b $rootfs/boot/mbr -p efi:=$esp_image -p freebsd:-"mkimg -s bsd -b $rootfs/boot/boot -p freebsd-ufs:=$image.part" -a 2 -o $image
-
-# cleanup
-
-rm $esp_image
-rm $image.part
-
-#xz -v -9 -T $(sysctl -n hw.ncpu) $image
-echo "[BOB] Done (output is in $image.xz)"
+sh create-image.sh $rootfs $src

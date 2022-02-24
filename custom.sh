4b92c26d7f442b377750fc64fcff8da1163f5c91
obiwac <obiwac@gmail.com>
2022-02-24 16:04:48 +0100

    main: remove old bob scripts

--- custom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# this is where any custom configuration of the final system goes
-# e.g., if you wanted to install 'vim', you'd add the following to this file:
-
-# pkg install -y vim

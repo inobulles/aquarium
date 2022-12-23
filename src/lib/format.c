bd1d023594c21e62f88e399cbd3641c99fb7917c
obiwac <obiwac@gmail.com>
2022-12-24 00:45:18 +0100

    libaquarium/format: Better confirmation

--- src/lib/format.c
+++ src/lib/format.c
@@ -134,8 +134,14 @@ int aquarium_format_new_table(aquarium_opts_t* opts, aquarium_drive_t* drive) {
 	char* const provider = drive->provider;
 
 	if (strncmp(provider, "md", 2)) { // XXX to be removed when releasing obviously
-		printf("Don't lol %s\n", provider);
-		return -1;
+		warnx("THIS OPERATION WILL MAKE IT DIFFICULT OR EVEN IMPOSSIBLE TO RECOVER YOUR DATA - ARE YOU SURE YOU WANT TO CONTINUE? (TYPE 'YES')");
+
+		char confirm[256];
+		fscanf(stdin, "%s", confirm);
+
+		if (strcmp(confirm, "YES")) {
+			return -1;
+		}
 	}
 
 	int rv = -1;

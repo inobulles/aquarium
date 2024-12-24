cb09d1bc8edd275c0e765f498722801fc6915dfd
Aymeric Wibo <obiwac@gmail.com>
2024-12-24 23:14:44 +0100

    sanctioned: Remove all the aquaBSD core stuff and just have FreeBSD 14.2-RELEASE

--- src/lib/sanctioned.h
+++ src/lib/sanctioned.h
@@ -1,20 +1,18 @@
 // This Source Form is subject to the terms of the AQUA Software License, v. 1.0.
-// Copyright (c) 2023 Aymeric Wibo
+// Copyright (c) 2024 Aymeric Wibo
 
 #pragma once
 
 static char const* const SANCTIONED =
+	// XXX Linux kernels are unsupported as of yet, so all Linux aquariums are base template only.
+	// Ubuntu 20.04.6 LTS (Focal Fossa).
+
 	"b:amd64.ubuntu.focal:https:github.com/inobulles/bob-linux-images/releases/download/amd64.ubuntu.focal/amd64.ubuntu.focal.txz:123438644:e1236bcc6a755a0db1d0fa34d4f6a942a56a51778a52d344c3d8c9c4f3b13682\n"
-	"b:amd64.aquabsd.0622a:https:github.com/inobulles/aquabsd-core/releases/download/v0622a-beta/base.txz:100050160:60321fefa8d46642f82fb51f9a1f16c552768da3f2b65b41ffa5fbaf8ff621fe\n"
-	"k:amd64.aquabsd.0622a:https:github.com/inobulles/aquabsd-core/releases/download/v0622a-beta/kernel.txz:46796444:da71214c6c6ed3de41599c2cef56c5215ec0c547eb839a49669e78598839a000\n"
-	"b:amd64.aquabsd.0922a:https:github.com/inobulles/aquabsd-core/releases/download/v0922a-beta/base.txz:100190248:7c6ddff808a8e3209917dd8beda46101a923b0b0386c2a1aef2bcba61a0406a0\n"
-	"k:amd64.aquabsd.0922a:https:github.com/inobulles/aquabsd-core/releases/download/v0922a-beta/kernel.txz:47538612:ad44de7c4d071db1862aadbbaf72be0551d736a89dbb9ad005cd0f95365b79b4\n"
-	"b:amd64.aquabsd.1222a:https:github.com/inobulles/aquabsd-core/releases/download/v1222a-beta/base.txz:100231832:3e32bbf8273dad00eb11607caa5618005eb97024687dadb68d947c48e11cf129\n"
-	"k:amd64.aquabsd.1222a:https:github.com/inobulles/aquabsd-core/releases/download/v1222a-beta/kernel.txz:47628512:7105e6c8f01d5e5c60d653f258356cb8a6a50330229753218c425c9bde9542e0\n"
-	"b:amd64.aquabsd.0.1.0-beta:https:github.com/inobulles/aquabsd-core/releases/download/v0.1.0-beta/base.txz:102059036:c433417808a9cbf19015427667c223744e16b2e57d89dad3f7d57c73a70d6a9c\n"
-	"k:amd64.aquabsd.0.1.0-beta:https:github.com/inobulles/aquabsd-core/releases/download/v0.1.0-beta/kernel.txz:53594932:515baf7ea7bc94e5473f2317b9d1343a7d405e3f2f51bf9abd93db529b05b126\n"
-	"b:amd64.aquabsd.0.1.1-beta:https:github.com/inobulles/aquabsd-core/releases/download/v0.1.1-beta/base.txz:106180020:c7503d62a8a4dd14e0a50bc0b9017cea1b45e8d9e03137b23bc62dde4f0f1fcc\n"
-	"k:amd64.aquabsd.0.1.1-beta:https:github.com/inobulles/aquabsd-core/releases/download/v0.1.1-beta/kernel.txz:47144808:6674891dfbdc6aa7b8ef04fda3330bbf76dd78d24d06f3909e0e8afe82c209a8\n"
+
+	// FreeBSD 14.2-RELEASE.
+
+	"b:amd64.freebsd.14-2-release:https:download.freebsd.org/ftp/releases/amd64/14.2-RELEASE/base.txz:205880752:e3971a3d4f36ed1ac67d2e7a5501726de79dd3695aa76bfad2a4ebe91a88a134\n"
+	"k:amd64.freebsd.14-2-release:https:download.freebsd.org/ftp/releases/amd64/14.2-RELEASE/kernel.txz:57859924:b441661d86cbea3be3191db462d0477e099e7dbdc4d2ca186ebb14df1a848480\n"
 ;
 
 // vim: nospell
